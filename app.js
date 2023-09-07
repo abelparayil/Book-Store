8acd54dcf5a671b396d958bc95ab135bff914a75
Abel Parayil Abraham <abelabr321@gmail.com>
2023-09-07 17:45:03 +0530

    Model created and tested

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+
+const app = express();
+
+app.use("/books", router);
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:AR1gI8TluPo2gspz@cluster0.v6kfsfw.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("connected to database"))
+  .then(() => {
+    app.listen(9000);
+  })
+  .catch((err) => console.log(err));
+// AR1gI8TluPo2gspz
