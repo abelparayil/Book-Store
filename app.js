ab6d81a0dc4ee751e59fe54d085f91d45a16b24d
Abel Parayil Abraham <abelabr321@gmail.com>
2023-09-08 23:09:10 +0530

    api created

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const router = require("./routes/book-routes");
 
 const app = express();
-
+app.use(express.json());
 app.use("/books", router);
 
 mongoose
@@ -15,4 +15,3 @@ mongoose
     app.listen(9000);
   })
   .catch((err) => console.log(err));
-// AR1gI8TluPo2gspz
---
+++ controllers/books-controller.js
@@ -0,0 +1,96 @@
+const Book = require("../model/Book");
+
+const getAllBooks = async (req, res, next) => {
+  let books;
+  try {
+    books = await Book.find();
+  } catch (err) {
+    console.log(err);
+  }
+
+  if (!books) {
+    return res.status(404).json({ message: "No products found" });
+  }
+  return res.status(200).json({ books });
+};
+
+const getById = async (req, res, next) => {
+  const id = req.params.id;
+  let book;
+  try {
+    book = await Book.findById(id);
+  } catch (err) {
+    console.log(err);
+  }
+  if (!book) {
+    return res.status(404).json({ message: "No book found" });
+  }
+  return res.status(200).json({ book });
+};
+
+const addBook = async (req, res, next) => {
+  const { name, author, description, price, available } = req.body;
+  let book;
+  try {
+    book = new Book({
+      name,
+      author,
+      description,
+      price,
+      available,
+    });
+    await book.save();
+  } catch (err) {
+    console.log(err);
+  }
+
+  if (!book) {
+    return res.status(500).json({ message: "unable to add" });
+  }
+  return res.status(201).json({ book });
+};
+
+const updateBook = async (req, res, next) => {
+  const id = req.params.id;
+  const { name, author, description, price, available } = req.body;
+
+  let book;
+  try {
+    book = await Book.findByIdAndUpdate(id, {
+      name,
+      author,
+      description,
+      price,
+      available,
+    });
+    book = await book.save();
+  } catch (err) {
+    console.log(err);
+  }
+  if (!book) {
+    return res.status(404).json({ message: "Unable to modify but this id" });
+  }
+  return res.status(200).json({ book });
+};
+
+const deleteBook = async (req, res, next) => {
+  const id = req.params.id;
+  let book;
+  try {
+    book = await Book.findByIdAndRemove(id);
+  } catch (err) {
+    console.log(err);
+  }
+  if (!book) {
+    return res
+      .status(404)
+      .json({ mesage: "Unable to delete the book with the id" });
+  }
+  return res.status(200).json({ book });
+};
+
+exports.getAllBooks = getAllBooks;
+exports.addBook = addBook;
+exports.getById = getById;
+exports.updateBook = updateBook;
+exports.deleteBook = deleteBook;
